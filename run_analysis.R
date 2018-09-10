c02808dbe1f014fa017ce5fcb2bceaec783fac01
Karl Holub <kholub@tripadvisor.com>
2018-09-10 02:25:49 -0400

    improved plotting

--- run_analysis.R
+++ run_analysis.R
@@ -41,6 +41,8 @@ for (plot_tree_depth in unique(parsed_results$depth)) {
       geom_line() +
       geom_point() +
       geom_errorbar(aes(ymin=bound_025, ymax=bound_975)) +
-      ggtitle(paste0('For depth ', plot_tree_depth))
+      ggtitle(paste0('For max tree depth ', plot_tree_depth)) +
+      xlab("Number of trees in ensemble") +
+      ylab("Observations per millisecond")
   ggsave(paste0('plots/depth', as.character(plot_tree_depth), '.png'), p)
 }
