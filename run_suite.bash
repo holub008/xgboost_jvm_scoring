80e7b809eb29ef4a2c559eea318549d6004bcd4a
Karl Holub <holub008@umn.edu>
2018-09-10 10:52:21 -0400

    supporting older versions of maven

--- run_suite.bash
+++ run_suite.bash
@@ -8,7 +8,8 @@ mkdir experiment_data/ pmml_models/ xgboost_raw_models/ plots/
 
 Rscript build_models.R
 
-mvn clean install -f xgb_score
-mvn exec:java@suite
+# note this forgoes simpler @ execution notation and the -f option to support older versions of maven common on CentOS
+(cd xgb_score && mvn clean install)
+(cd xgb_score/ &&  mvn exec:java -Dexec.mainClass="kh.experiments.xgb_score.ExperimentSuite" -Dexec.args="../xgboost_raw_models ../pmml_models ../experiment_data ../features.fmap ../results.csv 20")
 
 Rscript run_analysis.R
